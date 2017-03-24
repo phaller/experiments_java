660eceb890b5745e9995a022ad6b271e829337b3
Philipp Haller <hallerp@gmail.com>
2017-03-24 12:00:05 +0100

    Lambdas and factories

--- src/java8/Person.java
+++ src/java8/Person.java
@@ -1,7 +1,29 @@
 package java8;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import java.io.*;
+import java.nio.file.FileSystemNotFoundException;
+
+class CheckPersonFemaleOlderThan implements CheckPerson {
+	private int age;
+
+	CheckPersonFemaleOlderThan(int age) {
+		this.age = age;
+	}
+
+	public boolean test(Person p) {
+		return (p.gender == Person.Sex.FEMALE && p.getAge() >= age);
+	}
+}
+
+interface WriterFactory {
+	public Writer getWriter() throws IOException;
+}
 
 public class Person {
 
@@ -9,17 +31,63 @@ public class Person {
         MALE, FEMALE
     }
 
+    public Person(String name) {
+    		this.name = name;
+    		this.birthday = LocalDate.of(1970, 4, 15);
+    		this.gender = Sex.FEMALE;
+    }
+
+    public Person(String name, LocalDate born) {
+		this.name = name;
+		this.birthday = born;
+		this.gender = Sex.FEMALE;
+    }
+
+    public Person(String name, LocalDate born, Sex gender) {
+		this.name = name;
+		this.birthday = born;
+		this.gender = gender;
+		//this.writerFactory = new DBWriterFactory()
+    }
+
+    public Person(String name, LocalDate born, Sex gender, WriterFactory writerFactory) {
+		this.name = name;
+		this.birthday = born;
+		this.gender = gender;
+		this.writerFactory = writerFactory;
+    }
+
+    WriterFactory writerFactory;
     String name;
     LocalDate birthday;
     Sex gender;
     String emailAddress;
 
     public int getAge() {
-        return 42;
+        return LocalDate.now().getYear() - birthday.getYear();
+    }
+
+    public void writeInfoToDataStore() throws IOException {
+    	    Writer writer = null;
+    	    try {
+    			writer = //new PrintWriter(new FileWriter("output.txt"));
+    					 // construct writer that connects to a DB
+    					 // new DBWriter(<create DB connection>)
+    					writerFactory.getWriter();
+    			writer.write("" + name + ", " + getAge());
+    		} catch (FileSystemNotFoundException fnfe) {
+    			// TODO
+    		} catch (IOException ioe) {
+    			// TODO
+    		} finally {
+    			if (writer != null)
+    				writer.close();
+    		}
+    		
     }
 
     public void printPerson() {
-        // ...
+        System.out.println("Person(" + name + ", " + getAge() + ", " + gender + ")");
     }
 
     public static void printPersonsOlderThan(List<Person> roster, int age) {
@@ -29,4 +97,74 @@ public class Person {
             }
         }
     }
+
+    public static void printPersonsBetween(List<Person> roster, int ageFrom, int ageTo) {
+        for (Person p : roster) {
+            if (p.getAge() >= ageFrom && p.getAge() < ageTo) {
+                p.printPerson();
+            }
+        }
+    }
+
+    public static void printPersonsWithSelection(List<Person> roster, Predicate<Person> checker) {
+        for (Person p : roster) {
+            if (checker.test(p)) {
+                p.printPerson();
+            }
+        }
+    }
+
+    public static void processPersonsWithSelection(
+    		List<Person> roster,
+    		Predicate<Person> checker,
+    		Consumer<Person> action) {
+        for (Person p : roster) {
+            if (checker.test(p)) {
+                action.accept(p);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    		List<Person> roster = new ArrayList<Person>();
+    		roster.add(new Person("Anna"));
+    		roster.add(new Person("John", LocalDate.of(1998, 4, 20), Person.Sex.MALE));
+    		
+    		Person.printPersonsOlderThan(roster, 18);
+    		
+    		System.out.println("between:");
+    		Person.printPersonsBetween(roster, 18, 25);
+    		
+    		System.out.println("CheckPerson interface:");
+    		CheckPerson myChecker = new CheckPersonFemaleOlderThan(40);
+    		Person.printPersonsWithSelection(roster, myChecker);
+    		
+    		System.out.println("CheckPerson interface (2):");
+    		
+    		Person.printPersonsWithSelection(roster, new CheckPerson() {
+    			public boolean test(Person p) {
+    				return p.getAge() < 40 && p.gender == Person.Sex.MALE;
+    			}
+    		});
+    		
+    		System.out.println("CheckPerson using lambda expression");
+    		
+    		Person.printPersonsWithSelection(roster,
+    			(Person p) -> p.getAge() < 40 && p.gender == Person.Sex.MALE
+    			// boolean test(Person p) { return p.getAge() < 40 && p.gender == Person.Sex.MALE; }
+    		);
+    		
+    		System.out.println("Two lambda expressions");
+    		
+    		Person.processPersonsWithSelection(roster,
+    			(Person p) -> p.getAge() >= 40 && p.gender == Person.Sex.FEMALE,
+    			(Person p) -> {
+    				int age = p.getAge();
+    				System.out.println(p.name + ": " + age);
+    			});
+    }
 }
+
+interface CheckPerson extends Predicate<Person> {}
+
+
