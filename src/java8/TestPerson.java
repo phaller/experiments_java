12a991501bd1710818ebfe7f9c1c2529bf8a6bad
Philipp Haller <hallerp@gmail.com>
2017-03-29 14:20:29 +0200

    Add test

---
+++ src/java8/TestPerson.java
@@ -0,0 +1,42 @@
+package java8;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class TestPerson {
+
+	/* Must be runnable without internet connection! */
+	@Test
+	public void testOutput() {
+		StringWriter stringWriter = new StringWriter();
+		
+		WriterFactory testFactory = new WriterFactory() {
+			public Writer getWriter() {
+				return stringWriter;
+			}
+		};
+
+		Person p = new Person(
+				"Jim",
+				LocalDate.of(1970, 3, 2),
+				Person.Sex.MALE,
+				testFactory);
+
+		try {
+			p.writeInfoToDataStore();
+		} catch (IOException ioe) {
+			// ???
+		}
+
+		// check for correct output
+		assertEquals(stringWriter.toString(), "Jim, 47");
+	}
+
+}
